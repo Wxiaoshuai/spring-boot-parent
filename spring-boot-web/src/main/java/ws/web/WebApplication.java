f026600ed4227bdafa850f9c5a363dabc6baca44
xiaoshuai <1562254274@qq.com>
2020-07-16 13:02:55 +0800

    修改一点 by ws

--- spring-boot-web/src/main/java/ws/web/WebApplication.java
+++ spring-boot-web/src/main/java/ws/web/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ws.*.mapper")
+@MapperScan("ws.web.*.mapper")
 public class WebApplication {
 
     public static void main(String[] args) {
